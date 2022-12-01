0ccd6eca086ac0fac3fe7b1aa024b846e7cad4c2
jffelippe <jffelippe@hotmail.com>
2022-12-01 17:23:51 -0500

    Creación de Producto con servicios, reposotorio y controlador

--- peluqueria/src/main/java/com/ciclo4/peluqueria/controller/ProductController.java
+++ peluqueria/src/main/java/com/ciclo4/peluqueria/controller/ProductController.java
@@ -1,11 +1,10 @@
 package com.ciclo4.peluqueria.controller;
 
 import com.ciclo4.peluqueria.model.Product;
-import com.ciclo4.peluqueria.repository.ProductRepository;
 import com.ciclo4.peluqueria.service.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
