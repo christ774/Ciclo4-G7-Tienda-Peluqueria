2327a5c35c404016b51580c20ae990c718072d43
jffelippe <jffelippe@hotmail.com>
2022-12-01 16:59:10 -0500

    Entidad Producto creada con servicios, repositorio y controlador

--- peluqueria/src/main/java/com/ciclo4/peluqueria/model/Product.java
+++ peluqueria/src/main/java/com/ciclo4/peluqueria/model/Product.java
@@ -1,9 +1,25 @@
 package com.ciclo4.peluqueria.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("servicios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Product { //Tarea asignada a Juan Felipe
+    @Id
+    private String id;
+    private String corteHombre;
+    private String corteMujer;
+    private String manicure;
+    private String pedicure;
+    private String barba;
 }
